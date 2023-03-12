5d775f62d9ae59e0dd4d816a56d3839e07a519f2
HazelCC <110214855+HazelCC@users.noreply.github.com>
2023-03-12 12:28:47 -0700

    change some directory

--- websitedemo/classify.py
+++ websitedemo/classify.py
@@ -1,4 +1,5 @@
 import cohere
+import json
 co = cohere.Client('jY0mYl6MTtHnKLxMAPYBGg69rWrxt2CW9Kh0J1TJ')
 from cohere.classify import Example
 
@@ -6,11 +7,14 @@ examples=[  Example("I want to have lunch", "eat out"),  Example("I want to have
           Example("I want to do cardio", "gym buddy"),Example("I want some to teach me", "Looking for mentor"), Example("I want to learn css", "Looking for mentor"), Example("I want to learn skiing", "Looking for mentor")
           
           ]
-inputs=["I want to try the new cafe", "I want to go to the gym", "I want to learn piano"]
+inputs=["I want to eat apple"]
 
 response = co.classify(
   inputs=inputs,
   examples=examples,
 )
-print(response)
+classifications_dict = vars(response)
+# Write the response to a local JSON file
+with open('response.json', 'w') as f:
+    json.dump(response, f)
 
