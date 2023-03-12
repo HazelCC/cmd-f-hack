f30914878770c7ed9f8d9774b15270be8680e1d9
HazelCC <110214855+HazelCC@users.noreply.github.com>
2023-03-12 11:01:47 -0700

    update readme

--- websitedemo/Home/home.js
+++ websitedemo/Home/home.js
@@ -1,6 +1,17 @@
-function processUserInput() {
-    const userInput = document.getElementById("user-input").value;
-    console.log("User Input: " + userInput);
-  }
+const submitButton = document.querySelector('input[type=image]');
+const inputField = document.getElementById("submit-button");
+
+submitButton.addEventListener('click', function() {
+    // Create a new file with the current timestamp as the filename
+    const timestamp = new Date().toISOString().replace(/[:.]/g, '-');
+    const filename = `user-input-${timestamp}.txt`;
   
+    // Write user input to the file
+    const file = new Blob([userInput.value], {type: 'text/plain'});
+    const a = document.createElement('a');
+    a.href = URL.createObjectURL(file);
+    a.download = filename;
+    document.body.appendChild(a);
+    a.click();
+  });
   
